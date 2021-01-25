5c90f08fdff4d50363262dcd7662a2fef579ee10
James Simpson <edenofest@gmail.com>
2021-01-24 19:53:24 -0500

    feat: Fix plate caps

--- src/dactyl_keyboard/dactyl.clj
+++ src/dactyl_keyboard/dactyl.clj
@@ -272,26 +272,24 @@
   (apply union
          (conj (for [column columns
                row rows
-               :when (or (and (= column 0) (< row 3))
-                         (and (.contains [1 2] column) (< row 4))
-                         (.contains [3 4 5 6] column))]
+               :when (or (.contains [(+ innercol-offset 2) (+ innercol-offset 3)] column)
+                         (and (.contains [(+ innercol-offset 4) (+ innercol-offset 5)] column) extra-row (= ncols (+ innercol-offset 6)))
+                         (and (.contains [(+ innercol-offset 4)] column) extra-row (= ncols (+ innercol-offset 5)))
+                         (and inner-column (not= row cornerrow)(= column 0))
+                         (not= row lastrow))]
                (->> (sa-cap (if (and pinky-15u (= column lastcol) (not= row lastrow)) 1.5 1))
-                    (key-place column row)))
-               (list (key-place 0 0 (sa-cap 1))
-                 (key-place 0 1 (sa-cap 1))
-                 (key-place 0 2 (sa-cap 1))))))
+                    (key-place column row))))))
 
 (def caps-fill
   (apply union
          (conj (for [column columns
                row rows
-               :when (or (and (= column 0) (< row 3))
-                         (and (.contains [1 2] column) (< row 4))
-                         (.contains [3 4 5 6] column))]
-                 (key-place column row keyhole-fill))
-               (list (key-place 0 0 keyhole-fill)
-                 (key-place 0 1 keyhole-fill)
-                 (key-place 0 2 keyhole-fill)))))
+               :when (or (.contains [(+ innercol-offset 2) (+ innercol-offset 3)] column)
+                         (and (.contains [(+ innercol-offset 4) (+ innercol-offset 5)] column) extra-row (= ncols (+ innercol-offset 6)))
+                         (and (.contains [(+ innercol-offset 4)] column) extra-row (= ncols (+ innercol-offset 5)))
+                         (and inner-column (not= row cornerrow)(= column 0))
+                         (not= row lastrow))]
+                (key-place column row keyhole-fill)))))
 
 ;placement for the innermost column
 (def key-holes-inner
